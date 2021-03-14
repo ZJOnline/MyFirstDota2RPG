69143f97e6b4ef7ca433c80dfaa3f4701cf99f87
ZJOnline <776945478@qq.com>
2021-03-14 13:05:04 +0800

    Initial commit

---
+++ game/scripts/vscripts/addon_game_mode.ts
@@ -0,0 +1,12 @@
+require('lib/inline_localization_manager');
+
+import { GameMode } from "./game_mode";
+
+Object.assign(getfenv(), {
+    Activate: GameMode.Activate,
+    Precache: GameMode.Precache,
+});
+
+if (GameRules.Addon) {
+    GameRules.Addon.Reload();
+}
